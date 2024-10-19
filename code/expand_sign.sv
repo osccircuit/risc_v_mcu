299ea7272181887c0b9f93d25dc3c80de3990857
osc_circuit <popoveg18@gmail.com>
2024-10-19 22:30:18 +0300

    Расширение функционала
    Для команд sw производится переработка модуля расширения знака

--- code/expand_sign.sv
+++ code/expand_sign.sv
@@ -1,10 +1,11 @@
 module expand_sign(
-    input [11:0] i_data,
+    input [22:0] i_data,
+    input i_imm_src,
 
     output logic [31:0] o_expand_data
 );
 
-    assign o_expand_data[31:12] = i_data[11];
-    assign o_expand_data[11:0] = i_data;
+    assign o_expand_data[31:12] = i_data[22];
+    assign o_expand_data[11:0] = !i_imm_src ? i_data[22:11] : {i_data[22:16], i_data[4:0]};
 
 endmodule
