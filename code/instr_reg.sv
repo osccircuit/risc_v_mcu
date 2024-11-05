bbb4ecb437553690f46cf62d471436f6c544cf61
Egor Popov <popoveg18@gmail.com>
2024-11-05 16:26:45 +0300

    Ошибка  в регистре инструкций
    Выходному сигналу добавлено logic

--- code/instr_reg.sv
+++ code/instr_reg.sv
@@ -5,7 +5,7 @@ module instr_reg(
     input [31:0] i_instr,
     input i_enable,
 
-    output [31:0] o_instr
+    output logic [31:0] o_instr
 );
 
     always_ff @(posedge i_clk or negedge i_rst) begin
