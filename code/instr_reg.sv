d283f6ade620096f77f427712beb5a072aed7363
Egor Popov <popoveg18@gmail.com>
2024-11-05 16:14:50 +0300

    Регистр инструкций
    Добавление регистра для хранения текущей инструкции

---
+++ code/instr_reg.sv
@@ -0,0 +1,16 @@
+module instr_reg(
+    input i_clk,
+    input i_rst,
+
+    input [31:0] i_instr,
+    input i_enable,
+
+    output [31:0] o_instr
+);
+
+    always_ff @(posedge i_clk or negedge i_rst) begin
+        if (!i_rst) o_instr <= 0;
+        else if (i_enable) o_instr <= i_instr;
+    end
+
+endmodule
