bf9f71a4d049cb8ae4b53c8cbaf5ffdeeb579f1d
Egor Popov <popoveg18@gmail.com>
2024-11-05 18:03:55 +0300

    Счетчик команд
    Добавляется файл счетчика команд процессора

---
+++ code/pc.sv
@@ -0,0 +1,20 @@
+module pc (
+    input i_clk,
+    input i_rst,
+
+    input [31:0] i_pc_next,
+    input i_enable,
+
+    output logic [31:0] o_pc
+);
+
+    always_ff @(posedge i_clk or negedge i_rst) begin
+        if(!i_rst) begin
+            o_pc <= 32'h0;
+        end
+        else if (i_enable) begin
+            o_pc <= i_pc_next;
+        end
+    end
+endmodule
+
